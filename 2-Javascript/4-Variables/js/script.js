6257a11ea7fab1d8f665b790f3093fe141c2fbcd
Mostafa Othman <smogeq7@yahoo.com>
2021-09-16 10:15:26 +0200

    variables_1

---
+++ 2-Javascript/4-Variables/js/script.js
@@ -0,0 +1,74 @@
+// initialize variable
+var variableName;
+// console.log(variableName)
+// initialize a variable & asign a value to
+var variable = 0;
+// console.log(variable)
+// General form to define a variable:
+// var (variable name) = datatype
+// roles for variable name:
+// 1- not allowed to start with number
+// var 3nnn = 0
+var m2r = 4
+var mr4 = 5
+// var 333 = "fds"
+
+// 2- all operator charactors(anywhare)(+-/*><=&|%^!)
+// var hr%t = 20
+// var hr+hu = 10
+// var hr>op = 10
+
+// 3- no space between in variable name
+// var first name = "Mostafa"
+var  fname  = ""
+// 4- all special charactors are vorbiden inside a variable name
+// ($#@()[]{}.;,?""'`:~)
+// var fname[ff = 10
+// 5- keywords in javascript
+// var let = 2
+// var var =10
+/* Key words in Javascript
+break case catch class const continue debugger default
+delete do else enum export extends false finally for
+function if implements import interface in instanceof let
+new package private protected public return static super
+switch this throw true try typeof var void while with yield 
+*/
+// var for
+var first_name = "My name"
+
+// recommanded use camel case & not capitalletter in begin
+var firstName = ""
+var abcdefghijklmnopqrstuvwxyz = 500
+// console.log(abcdefghijklmnopqrstuvwxyz)
+var wasserStuffBrueckenVerbindung = "HO2"
+// console.log(wasserStuffBrueckenVerbindung)
+var firstName_1 = 10
+var BMW = "bmwx5"
+var bmw = "bmwx6"
+////////////////////////////////////
+// console.log(x)undefined
+var x = 5
+x = 10
+console.log(x)//=>5
+console.log(typeof x)//=> number
+x = "t"// assign a new value to x
+console.log(x)
+console.log(typeof x)
+x = 12
+console.log(x)
+x = x + 2
+console.log(x)// 14
+// increment(++)
+// x = x + 1
+// increment +1
+x ++;
+x ++;
+console.log(x)//=>16
+// increment with a specific value (variable += value)
+x += 20
+console.log(x)//=>36
+
+
+
+
