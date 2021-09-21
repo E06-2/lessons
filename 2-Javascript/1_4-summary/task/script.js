4d64a0f0bf1f640610028f1cbc1ff97d414bd2ab
mt-sette <matteo.sette@digitalcareerinstitute.org>
2021-09-21 11:28:47 +0200

    solution

---
+++ 2-Javascript/1_4-summary/task/script.js
@@ -0,0 +1,24 @@
+console.log("this is an exercise in console logging");
+
+var message = "This is a variable";
+
+console.log(message);
+
+
+var firstName = "John";
+
+var lastName = "Smith";
+
+console.log(firstName, lastName);
+
+var age = 24;
+
+var city = "Berlin";
+
+console.log(
+    `${firstName} is ${age} years old,
+    he lives in ${city}
+`
+);
+
+console.log(firstName + " is " + age + " years old \n and he lives in " + city);
