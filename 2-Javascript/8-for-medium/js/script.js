453f078d3527dd7ef260149b6254eb4278d12da3
ahmad-teaching <ahmad.osman@digitalcareerinstitute.org>
2021-09-23 14:59:46 +0200

    for loop stars pattern

--- 2-Javascript/8-for-medium/js/script.js
+++ 2-Javascript/8-for-medium/js/script.js
@@ -19,7 +19,85 @@
 // 8
 // 10
 
-for(let i = 2; i <= 10; i += 2) {
-    document.write(i);
+// for(let i = 2; i <= 10; i += 2) {
+//     document.write(i);
+//     document.write('<br>');
+// }
+
+// let c = 5;
+// c +=  2; // c = c + 2;
+// let v = c++;
+// console.log(v);
+// console.log(c);
+
+// for(let i = 0; i < 10; i++) {
+//     for(let x = 0; x < 10; x++){
+//         document.write('*');
+//     }
+//     document.write('<br>');
+// }
+
+// task: change the previous code to print the following pattern
+
+// *
+// **
+// ***
+// ****
+// *****
+// ******
+// *******
+// ********
+// *********
+// **********
+
+// for(let i = 1; i <= 10; i++) {
+//     for(let x = 0; x < i; x++){
+//         document.write('*');
+//     }
+//     document.write('<br>');
+// }
+
+// loop1 i = 1 
+// loop2 0 --> 1 * <br>
+
+// loop1 i = 2
+// loop2 0 --> 2 ** <br>
+
+// loop1 i = 3
+// loop2 0 --> 3 *** <br>
+
+// loop1 i = 4
+// loop2 0 --> 4 **** <br>
+
+// task: write code to print the following pattern:
+
+// **********
+// *********
+// ********
+// *******
+// ******
+// *****
+// ****
+// ***
+// **
+// *
+
+for(let i = 10; i >= 1; i--) {
+    for(let x = 0; x < i; x++){
+        document.write('*');
+    }
     document.write('<br>');
 }
+
+// task: write code to print the following pattern:
+
+// ---------*
+// --------**
+// -------***
+// ------****
+// -----*****
+// ----******
+// ---*******
+// --********
+// -*********
+// **********
