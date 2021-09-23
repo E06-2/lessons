53df44f51f7706e62b234f0a8c135d4d5a94c4c5
ahmad-teaching <ahmad.osman@digitalcareerinstitute.org>
2021-09-23 12:15:22 +0200

    for loop medium

---
+++ 2-Javascript/8-for-medium/js/script.js
@@ -0,0 +1,25 @@
+// loop to print numbers above each other from 0 to 9
+// for(let i = 0; i < 10; i++) {
+//     document.write(i);
+//     document.write('<br>');
+// }
+
+// make changes on the following loop to print numbers from 9 to 0
+// for(let i = 9; i >= 0; i--) {
+//     document.write(i);
+//     document.write('<br>');
+// }
+
+// write a for loop to print even numbers 
+// above each other from 1 to 10 WITHOUT using if statement
+
+// 2
+// 4
+// 6
+// 8
+// 10
+
+for(let i = 2; i <= 10; i += 2) {
+    document.write(i);
+    document.write('<br>');
+}
