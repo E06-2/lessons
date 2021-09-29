90389e7d2ed881aa1feafe6fbb50d4a3a9e6bc5b
Mostafa <mostafa.othman@digitalcareerinstitute.org>
2021-09-29 11:15:38 +0200

    table_loop_ex

---
+++ 2-Javascript/10-while-loop-scratch/js/ex1.js
@@ -0,0 +1,44 @@
+// x, y to cells, rows
+var rows = 4
+var cells = 4
+// somthing repeat 3 times: start from 0 == < 3
+// 0    1   2
+// [0, 5] ==> 6
+var counter = 1
+document.write(`<table>`)
+for(let y = 0; y < rows; y++){// 3
+    document.write(`<tr>`)
+    // cells area
+    for(let x = 0; x < cells; x++){//4
+        if(y == x ){
+            document.write(`<td style="background:red">`)
+        }else{
+            document.write(`<td>`)
+        }
+        document.write(`(${y}, ${x})`)
+        document.write(`</td>`)
+        counter++
+    }
+    document.write(`</tr>`)
+}
+document.write(`</table>`)
+//////////////////////////////////////////////////
+document.write(`<hr>`)
+
+document.write(`<table>`)
+for(let y = 0; y < rows; y++){// 3
+    document.write(`<tr>`)
+    // cells area
+    for(let x = 0; x < cells; x++){//4
+        if(y == x ){
+            document.write(`<td style="background:red">`)
+        }else{
+            document.write(`<td>`)
+        }
+        document.write(`(${y}, ${x})`)
+        document.write(`</td>`)
+        counter++
+    }
+    document.write(`</tr>`)
+}
+document.write(`</table>`)
