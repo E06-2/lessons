cb79cc937560c49cff2af188b9977f7e270cd29b
ahmad-teaching <ahmad.osman@digitalcareerinstitute.org>
2021-09-30 11:47:07 +0200

    function task

--- 2-Javascript/10-while-loop-scratch/js/script.js
+++ 2-Javascript/10-while-loop-scratch/js/script.js
@@ -12,16 +12,16 @@
 // write a code to let user enter texts and show this text on the page
 // until the user enter empty text so the prompt should stop
 
-let check = true;
-while(check) {
-    let msg = prompt('Enter your text');
-    // check if message is empty string
-    if (msg == '') {
-        check = false;
-    } else {
-        document.write(`${msg} <br>`)
-    }
-}
+// let check = true;
+// while(check) {
+//     let msg = prompt('Enter your text');
+//     // check if message is empty string
+//     if (msg == '') {
+//         check = false;
+//     } else {
+//         document.write(`${msg} <br>`)
+//     }
+// }
 
 // Home Work :  
 // write a code to allow user to enter  numbers using prompt and 
@@ -38,3 +38,31 @@ while(check) {
 // the program should show result like this:
 // The Total is: []
 // The Average is: []
+
+let i = 0; // number of the entries numbers that user will enter
+let sum = 0;
+let check = true;
+while (check) {
+    // check only the string entry
+    let entry = prompt('Enter a number ' + (i + 1));
+    if (entry === '' || Number.isNaN(entry) || entry.includes(' ')) {
+        check = false;
+    } else {
+        let number = Number(entry);
+        sum += number;
+        i++;
+    }
+}
+// if i is 0 that means user did not enter any number
+// so we should show no result
+if (i === 0) {
+    document.write(`No entries`);
+} else {
+    document.write(`The Total is : ${sum} <br>`);
+    document.write(`The average is : ${sum / i} <br>`);
+}
+
+
+// 1 , 2 , 3 , ' '
+// sum = 6
+// average = 6 / 4 wrong it supposed to be 6 / 3 
---
+++ 2-Javascript/12-falsy/js/script.js
@@ -0,0 +1,26 @@
+// let x = '2';
+// let z = 2;
+
+// // == only will check the value NOT the type
+// // === will check the value AND the type
+// if (x === z) {
+//     console.log('they are equal');
+// } else {
+//     console.log('they are NOT equal');
+// }
+
+// 0 : false
+// '': false
+// null: false
+// undefined : false
+// NaN: false
+// false: false
+
+let d = NaN;
+if (d) {
+    console.log('it is true');
+} else {
+    console.log('it is false');
+}
+
+
