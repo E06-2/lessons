56ade3f0c5d088dbf22668888f238ace3b6f7913
mt-sette <matteo.sette@digitalcareerinstitute.org>
2021-09-30 13:29:31 +0200

    function calculator

--- 2-Javascript/13-functions/js/script.js
+++ 2-Javascript/13-functions/js/script.js
@@ -14,16 +14,16 @@ function sum(a, b) {
     let z = a + b;
     //console.log(z);
     return z;
-    
+
 }
 
-console.log(sum(2, 3));
-console.log(sum(5, 6));
+// console.log(sum(2, 3));
+// console.log(sum(5, 6));
 
 
-let x = 5 * sum(3 , 4);
+let x = 5 * sum(3, 4);
 
-console.log(x);
+// console.log(x);
 
 
 // task: 
@@ -38,3 +38,47 @@ console.log(x);
 // call these function using switch case 
 // for example if user entered + you need to call sum function
 // or if user entered - you need to call reduce function and so on
+
+let number1 = Number(prompt("Enter first number: "));
+let operator = prompt("Enter operator number: ");
+let number2 = Number(prompt("Enter Second number: "));
+let result = 0;
+
+// console.log(number1, operator, number2);
+
+function sum(a, b) {
+    return a + b;
+}
+function multiply(a, b) {
+    return a * b;
+}
+function divide(a, b) {
+    return a / b;
+}
+function reduce(a, b) {
+    return a - b;
+}
+function percentage(a, b) {
+    return multiply(divide(number1, number2), 100)
+    // return a * (100 / b);
+}
+
+switch (operator) {
+    case "+":
+        result = sum(number1, number2);
+        break;
+    case "-":
+        result = reduce(number1, number2);
+        break;
+    case "*":
+        result = multiply(number1, number2);
+        break;
+    case "/":
+        result = divide(number1, number2);
+        break;
+    case "%":
+        result = percentage(number1, number2);
+        break;
+}
+
+console.log("The result is: ", result);
