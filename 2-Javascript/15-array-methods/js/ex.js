763683692aa0f0345f37d84f0306ff4865323078
Mostafa <mostafa.othman@digitalcareerinstitute.org>
2021-10-06 10:30:39 +0200

    setTimeout-interval

--- 2-Javascript/15-array-methods/js/ex.js
+++ 2-Javascript/15-array-methods/js/ex.js
@@ -47,6 +47,7 @@ document.write(`
  * factorial number 5!= 5 * 4 * 3 * 2 * 1 
  * factorial number 7!= 7 * 6 * 5 * 4 * 3 * 2 * 1
  * factorial number 0!= 1
+ * factorial number 4! = 4 * 3 * 2 * 1
  * factorial number 4! = 4 * 3!
  * factorial number 3! = 3 * 2 * 1
  * factorial number 5! = 5 * 4!
@@ -68,7 +69,61 @@ function recursionFactorial(n){
         return 1
     }else{
         return n * recursionFactorial(n - 1)
+        // when n =5
+        // return 5 * (4 * (3 * (2 * ( 1 * (1)))))
+        //  (5 * (5 - 1) * (4 - 1) * (3 - 1) * (2 - 1)) = 5 * 4 * 3 * 2 * 1 = 120
     }
 }
-factorial(7)
-console.log(recursionFactorial(7))
+factorial(5)
+console.log(recursionFactorial(5))
+/////////////////////////////////////////////
+let sum = 0
+for (let i = 0 ; i <= 5; i++){
+    //sum += i
+    sum = sum + i
+}
+console.log(sum)
+// n should be positive not 0
+
+function recursiveSum(n){
+    if(n <= 0){
+        return 0
+    }else{
+        return n + recursiveSum(n - 1)
+    }
+}
+console.log(recursiveSum(5))
+////////////////////////////////////////
+// special function in javascript
+/**
+ * setTimeout(procedur (function), delay time in ms)
+ */
+// function cool(){
+//     console.log("Hey am cool function")
+//     console.log("This log after 3 second")
+
+//     // alert("this works after 3 s")
+// }
+// setTimeout(function(){
+//     console.log("This log after 3 second")
+//     //alert("this works after 3 s")
+// }, 3000)
+
+// setTimeout(cool, 5000)
+
+setTimeout(function(){
+    console.log("my setTimeout function is started.")//wait after 5s
+    setTimeout(function(){
+        console.log("Humm this is setTimeout, inside setTimeout")//wait after 5s + 2s = 7s
+    },2000)
+}, 5000)
+
+console.log("This log is after setTimeout function")
+/**
+ * setInterval(procedur (function), 1000 ms = 1s)
+ */
+let ii = 0
+setInterval(function(){
+    ii++
+    console.log(ii)
+}, 1000)
