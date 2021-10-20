3b30feed80b499c489be4cc62f547b0e6cc0141c
Mostafa <mostafa.othman@digitalcareerinstitute.org>
2021-10-20 12:15:35 +0200

    detructuring_task2

--- 2-Javascript/22-Objects/js/destructure.js
+++ 2-Javascript/22-Objects/js/destructure.js
@@ -5,7 +5,8 @@ const obj = {
     address: {
         city: "Berlin",
         street: "Vectory Strasse"
-    }
+    },
+    skills: ["Javascript", "Python", "Math"]
 }
 // Object.keys(obj)==> [all keys inside obj]
 console.log(Object.keys(obj))
@@ -19,9 +20,8 @@ console.log(Object.entries(obj)[2])
 // let lname = obj.lastName
 // let address, lastName;
 // ({address, lastName } = obj)
-let {address, lastName } = obj
+let {lastName } = obj
 console.log(lastName)
-console.log(address)
 let {a, c} = {a:"s", b: 10, c: 200}
 console.log(a, c)
 // let x, y
@@ -31,3 +31,28 @@ console.log(a, c)
 // console.log(y)
 
 // define three variables from obj, (firstname, age, address)
+// let {address, firstName, age} = obj
+let {age: age, address: address, firstName: firstName} = obj
+console.log(firstName, age, address)
+// let address = {
+//     city: 10, street: true
+// }
+// define city, street fom obj
+// let {city, street} = obj.address
+//let {address: {city, street}} = obj
+let {"city": myCity, "street": myStreet} = obj.address
+console.log(myCity, myStreet)
+
+
+const obj2 = {
+    score: 50,
+    "my-avarage": 100,
+    result: true
+}
+
+let {score: myScore, result: myResult} = obj2
+// let {key: variable} = object
+console.log(myScore, myResult)
+// let {my-avarage} = obj2 ==> syntax error
+let {"my-avarage": avar} = obj2
+console.log(avar)
