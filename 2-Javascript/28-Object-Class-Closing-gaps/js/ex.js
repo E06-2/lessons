4c9eb58c5adbfb6b29a033ef3a49286ecdb34624
Mostafa <mostafa.othman@digitalcareerinstitute.org>
2021-10-27 09:58:45 +0200

    Palindrome_function

---
+++ 2-Javascript/28-Object-Class-Closing-gaps/js/ex.js
@@ -0,0 +1,64 @@
+// Palindorme Word Checker
+/*
+    Palindorme word is any word readble from right to left, left to right
+    ex: Anna, HuuH, deded
+    Not Palindorme: Blabla, Hummer, BMW
+*/
+/**
+ * create a function isPalindorme(word: string): true if Palindorme or false if not
+ */
+
+
+/**
+ * A    N   N   N   A   ==> 5
+ * 1- loop for all letters
+ *      a- check first with last
+ *      c- after first with before last
+ *      d- .............
+ *      if first NOT equals last
+ *          return false
+ * return true
+ * @param {*} word 
+ */
+function isPalindrome(word){
+    for(let i = 0; i < word.length/2; i++){
+        // console.log(word[i], word[word.length -1  - i])
+        if(word[i].toLowerCase() !==  word[word.length -1  - i].toLowerCase())
+            return false
+    }
+    return true
+}
+let w = "Mostafa"
+console.log(isPalindrome(w))
+
+// reverse string
+let rev = w.split("").reverse().join("")
+console.log(rev)
+/**
+ * create a function getPalendrome(text: string): array of all Palindrome words inside the text
+ * 1- take the text
+ * 2- define array to store
+ * 3- loop each word (convert to array using split(" "))
+ *      check if each word is Palindrome (we can use our function isPalindrome)
+ *          if palindrome: save it in extrnal array
+ * 4- return this array (store)
+ */
+function getPalendrome(text){
+    let store = []
+    let arrayOfWords = text.split(" ")
+    arrayOfWords.forEach(word=>{
+        if(word.trim().length > 1)
+            if(isPalindrome(word)){
+                store.push(word)
+            }
+    })
+    return store
+}
+
+let t = "    Lorem ipsum dolor sit amet, consectetur i BEEB adipisicing elit. Accusantium, doloribus esse deserunt ad vero unde, cumque voluptatibus, nemo  officia AnnA exercitationem   eum. Dolores huuh ad qui animi autem nobis, aliquid maxime earum."
+let result = getPalendrome(t)
+console.clear()
+console.log(result)
+// console.clear()
+// let str = "         this is my text    I          "
+// console.log(str.trim())
---
+++ 3-Javascript-tasks/1-basic-level/1-datatypes/js/script.js
@@ -0,0 +1,56 @@
+/**
+ * Task 1
+ * Try to display the followning information about you in console section in browser:
+ *      1- Your First Name
+ *      2- Your Last Name
+ *      3- Your Age
+ *      4- Your Mother Language
+ *      5- Married
+ *      6- Your Height
+ *      7- Your Skills
+ *      8- Your Favorite Foods
+ * 
+ * Use console.log() to display.
+ */
+
+/**
+ * Task 2
+ * Try to replace <stuff> inside console.log(typeof stuff) to display in console the following:
+ *      1- string
+ *      2- number
+ *      3- boolean
+ *      4- object
+ *      5- undefiened
+ * 
+ *      ex: console.log(typeof "blabla")===>string
+ */
+
+/**
+ * Task 3
+ *      Which one is Not a Number:
+ *      1- 360
+ *      2- 002
+ *      3- 0.02 
+ *      4- -500 
+ *      5- 0.00 
+ *      6- (5 + 1)
+ *      7- "523"
+ *      8- null
+ *      9- Infinity
+ *      10- undefiend
+ *      11- 0b1010
+ *      12- 0b52
+ *      13- 0xFF
+ *      14- 0xFY
+ *      15- 5,1
+ *      16- 0o56
+ *      17- '2'
+ *      18- 0B10110
+ *      19- 0X410FfaA
+ *      20- 0XFe10R
+ *      21- NaN
+ *      22- true
+ */
+
+
+
