02fbba23602b373dd71ab688303ac688d18762f0
ahmad-teaching <ahmad.osman@digitalcareerinstitute.org>
2021-10-28 14:28:38 +0200

    play sound

--- 2-Javascript/31-exercises/js/script.js
+++ 2-Javascript/31-exercises/js/script.js
@@ -1,5 +1,14 @@
 const container = document.querySelector('#container');
 
+// create audio element to play explosion sound 
+const explosionSound = document.createElement('audio');
+explosionSound.src = './sounds/explod.mp3';
+explosionSound.setAttribute('controls', 'none');
+explosionSound.setAttribute('preload', 'auto');
+explosionSound.style.display = 'none';
+
+container.append(explosionSound);
+
 // create click event listener for container element
 container.addEventListener('click', e => {
     //console.log(e.clientX, e.clientY);
@@ -11,7 +20,7 @@ container.addEventListener('click', e => {
     // set left style for tha new div
     ball.style.left = (e.clientX - 15) + 'px';
     // set the top style for the new div
-    ball.style.top = (e.clientY - 15) + 'px';
+    ball.style.top = (e.clientY - 20) + 'px';
     // add the ball div to container
     container.append(ball);
     move(ball);
@@ -29,8 +38,13 @@ function move (ball) {
         startTop += 5;
         ball.style.top = startTop + 'px';
         // check if the ball reaches the bottom edge of the container
-        if ((containerHeight - 30) <= startTop) {
+        if ((containerHeight - 40) <= startTop) {
             clearInterval(pid);
+            // change the background image of ball
+            ball.style.backgroundImage = "url('../images/explod.png')";
+            explosionSound.pause();
+            explosionSound.currentTime = 0;
+            explosionSound.play();
         }
     }, 15)
 }
