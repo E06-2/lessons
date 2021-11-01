8500760c9d1e66c6daa9e9a61c66b85330533394
mt-sette <matteo.sette@digitalcareerinstitute.org>
2021-11-01 19:22:25 +0100

    Tasks for regex

---
+++ 2-Javascript/32-regex/task/index.js
@@ -0,0 +1,34 @@
+// Break camelCase
+//
+// Complete the solution so that the function will break up camel casing
+// using a space between words
+//
+//  "camelCasing"  =>  "camel Casing"
+//  "identifier"   =>  "identifier"
+//  ""             =>  ""
+
+const breakCamelCase(str) => {
+    //TODO
+    return str
+}
+
+
+// Vowel remover
+// Given a string or text, remove all the vowel (aeiou) case insennsitive.
+// For example, the string:
+// "This website is for losers LOL!" would become "Ths wbst s fr lsrs LL!".
+
+const disemvowel(str) => {
+    //TODO
+    return str
+}
+
+// Filter information from URL
+// Given string of text retung the URL domain name
+//
+// https://google.com => google
+
+const domainExtractor(ulr) => {
+    //TODO
+    return ulr
+}
