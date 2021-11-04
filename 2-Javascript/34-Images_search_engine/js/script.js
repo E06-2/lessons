ade389d334fee0f6d5c10bab82f879969be098f0
ahmad-teaching <ahmad.osman@digitalcareerinstitute.org>
2021-11-04 13:29:01 +0100

    async await fetch

--- 2-Javascript/34-Images_search_engine/js/script.js
+++ 2-Javascript/34-Images_search_engine/js/script.js
@@ -1,12 +1,42 @@
- // get required DOM Elements
- const searchWordInp = document.getElementById('searchWordInp');
- const searchBtn = document.getElementById('searchBtn');
- const resultSection = document.getElementById('resultSection');
+// get required DOM Elements
+const searchWordInp = document.getElementById('searchWordInp');
+const searchBtn = document.getElementById('searchBtn');
+const resultSection = document.getElementById('resultSection');
 
-    // add event listener to search button
-    searchBtn.addEventListener('click', () => {
-        //alert(searchWordInp.value)
-        const url = 'https://pixabay.com/api/?'+
-                    'key=23836825-d28900a4f5025893278996a18'+
-                    '&q='+searchWordInp.value;
-    })
+// add event listener to search button
+searchBtn.addEventListener('click', async () => {
+    //alert(searchWordInp.value)
+    const url = 'https://pixabay1.com/api/?' +
+        'key=23836825-d28900a4f5025893278996a18' +
+        '&q=' + searchWordInp.value;
+    resultSection.textContent = 'Loading...';
+    
+    try {
+        const response = await fetch(url);
+        const data = await response.json();
+        console.log(data);
+        resultSection.textContent = 'we got the data';
+    } catch (error) {
+        resultSection.textContent = 'there is an error with getting the data';
+    }
+
+    // fetch(url).then(response => {
+    //     if (resposonse.status === 200) {
+    //         response.json().then(data => {
+    //             console.log(data);
+    //         }).catch(error => {
+    //             resultSection.textContent = 'Wrong Data has been returned';
+    //         })
+
+    //     } else {
+    //         resultSection.textContent = 'A server Error accrued while getting the Data';
+    //     }
+
+    // }).catch(error => {
+    //     resultSection.textContent = 'An Error accrued while getting the Data';
+    // })
+})
+
+// new concepts:
+// async await
+// try catch
