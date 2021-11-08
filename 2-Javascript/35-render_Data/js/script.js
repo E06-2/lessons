e6398f84a1569272be887ff16d85b42f0fbcf9a1
Mostafa <mostafa.othman@digitalcareerinstitute.org>
2021-11-08 11:45:03 +0100

    getPostTask1

--- 2-Javascript/35-render_Data/js/script.js
+++ 2-Javascript/35-render_Data/js/script.js
@@ -9,3 +9,50 @@
  * use grid, or flex or float to order this posts,
  * make it like a function getPosts()
  */
+
+/**
+ * when document loaded, do some procedures
+ */
+window.onload = ()=>{
+    // console.log("Document loaded")
+    // create content div
+    const contentDiv = document.createElement("div")
+    // getting container div
+    const container = document.getElementsByClassName('container')[0]
+    // put contentDiv inside container div
+    container.append(contentDiv)
+    // style the contentDiv
+    // contentDiv.style.display = "grid"
+    contentDiv.classList.add("content")
+
+    // container.appendChild(contentDiv)
+    getPosts()
+}
+function getPosts(){
+    fetch('https://jsonplaceholder.typicode.com/posts').then(response=>{
+        // console.log(response)
+        response.json().then(data=>{
+            console.log(data)
+            //1- loop for each object (POSTS) inside
+                // TODO
+                //a- for each object(POST) inside, create anew div (postConteiner)
+                // TODO
+                //b- create h2 element, put inside it the title of the POST
+                // TODO
+                //c- put this h2 element inside (postConteiner)
+                // TODO
+                //d- put this postConteiner inside (contentDiv)
+                // TODO
+
+        }).catch(er=>{
+            console.log(er)
+        })
+    }).catch(error=>{
+        console.log(error)
+    })
+}
+// Promise.then(data=>{
+//     // no error
+// }).catch(errrror=>{
+// // handle error
+// })
