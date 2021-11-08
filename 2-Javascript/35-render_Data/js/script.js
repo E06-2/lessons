ba7dc4610ba3b76d63bf78657ab0f2ba05b59a47
Mostafa <mostafa.othman@digitalcareerinstitute.org>
2021-11-08 09:38:19 +0100

    getPostsTask

---
+++ 2-Javascript/35-render_Data/js/script.js
@@ -0,0 +1,11 @@
+/**
+ * Getting Data from:
+ * https://jsonplaceholder.typicode.com
+ */
+
+/**
+ * 0-Task
+ * when page loaded, display all posts like boxes, contains the title of this post
+ * use grid, or flex or float to order this posts,
+ * make it like a function getPosts()
+ */
