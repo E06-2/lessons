0d4242f9439cd9d2cf45412b526496f00953bdb2
mt-sette <matteo.sette@digitalcareerinstitute.org>
2021-11-09 10:50:44 +0100

    task

---
+++ 2-Javascript/36-dom-revision/task/index.js
@@ -0,0 +1,20 @@
+// Assign all elements
+
+
+// Change border of ID demo to purple
+
+
+
+// Change border of class demo to orange
+
+
+
+// Change border of tag demo to blue
+
+
+
+// Change border of ID demo-query to red
+
+
+
+// Change border of class query-all to green
