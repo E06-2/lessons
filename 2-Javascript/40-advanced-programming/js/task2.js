799ac88a7790fcedaf017cf2df1fae6ea54355c8
Mostafa <mostafa.othman@digitalcareerinstitute.org>
2021-11-22 14:30:35 +0100

    prototypes

--- 2-Javascript/40-advanced-programming/js/task2.js
+++ 2-Javascript/40-advanced-programming/js/task2.js
@@ -10,26 +10,57 @@
  * 2- Add to your data, average
  */
 
-const john = {
-    HTML: 50,
-    javascript: 40,
-    css: 30,
-    react:70,
-    nodejs: 90,
-    bootstrap:64,
-    average: function(){
+let fff={
+    t:4, p:5
+}
+/**
+ * add average, success procedures for each object I will create in This file
+ *      use (DATA_STRUCTURE).prototype.anything= ...
+ * to remove prototype:
+ * // delete (DATA_STRUCTURE).prototype.anything
+ */
+Object.prototype.average = function(){
         let sum = 0
         let counter = 0
         for(let k in this){
             // execlude average from looping
-            if(k !== "average"){
+            // if(k !== "average" && k !== "success"){
+            if(typeof this[k] === "number"){
                 counter++;
                 sum += this[k]
             }
             // console.log(k)
         }
         return sum/counter
-    }
+}
+// Object.prototype.mostafa = "I've just create this prototype :)"
+
+const john = {
+    HTML: 50,
+    javascript: 40,
+    css: 30,
+    react:30,
+    nodejs: 20,
+    bootstrap:14,
+    // average: function(){
+    //     let sum = 0
+    //     let counter = 0
+    //     for(let k in this){
+    //         // execlude average from looping
+    //         // if(k !== "average" && k !== "success"){
+    //         if(typeof this[k] === "number"){
+    //             counter++;
+    //             sum += this[k]
+    //         }
+    //         // console.log(k)
+    //     }
+    //     return sum/counter
+    // },
+    // success: function(){
+    //     if(this.average() >=50)
+    //         return true
+    //     return false
+    // }
 }
 /**
  * add procedure to average, so that all attributes(keys) need to be counted and return some value
@@ -38,7 +69,57 @@ const john = {
  */
 john.sass = 43
 console.log(john.average())
-john.java = 90
+// john.java = 90
+// console.log(john.average())
+// john.math = 100
 console.log(john.average())
-john.math = 100
+
+/**
+ * add another attribute (success) so that if average > 50 true, false
+ */
+// console.log(john.success())
+john['c++'] = 100
+// john.c++ = 100
+john['c'] = 100// john.c = 
+john['assembly'] = 100
+console.log(john.average())
+console.log(john)
+console.log(john['c++'])
+///////////////////////////////////////////
+/**
+ * define another guy with grades
+ */
+const smith = {
+    HTML: 55,
+    css:40,
+    // average: function(){
+    //     let sum = 0
+    //     let counter = 0
+    //     for(let k in this){
+    //         // execlude average from looping
+    //         // if(k !== "average" && k !== "success"){
+    //         if(typeof this[k] === "number"){
+    //             counter++;
+    //             sum += this[k]
+    //         }
+    //         // console.log(k)
+    //     }
+    //     return sum/counter
+    // },
+    // success: function(){
+    //     if(this.average() >=50)
+    //         return true
+    //     return false
+    // }
+}
+console.clear()
+console.log(john.average())
+console.log(smith.average())
+let foo ={
+    t:4,
+    g:55
+}
+console.clear()
+console.log(foo.average())
+// delete Object.prototype.average
 console.log(john.average())
---
+++ 2-Javascript/40-advanced-programming/js/task3.js
@@ -0,0 +1,15 @@
+/**
+ * Add a new ptototype for Object, named: info
+ *      this should analysis the object and return an object with following info:
+ *      {
+ *      numbers: count of numbers in this object,
+ *      strings: count of strings in this object,
+ *      booleans: count of booleans in this object,
+ *      arrays: count of arrays in this object,
+ *      objects: count of objects in this object,
+ *      }
+ * test it when finish
+ */
+String.prototype.hummer = function(){console.log("This is 'this': "+this)}
+// "mostafa".split("t").forEach()
+"sss".hummer()
