bd892897529efed25e8af227acf0fefd8887bf5c
Mostafa <mostafa.othman@digitalcareerinstitute.org>
2021-11-22 15:48:47 +0100

    prototype-task

--- 2-Javascript/40-advanced-programming/js/task3.js
+++ 2-Javascript/40-advanced-programming/js/task3.js
@@ -10,6 +10,73 @@
  *      }
  * test it when finish
  */
-String.prototype.hummer = function(){console.log("This is 'this': "+this)}
-// "mostafa".split("t").forEach()
-"sss".hummer()
+// String.prototype.hummer = function(){console.log("This is 'this': "+this)}
+// // "mostafa".split("t").forEach()
+// "sss".hummer()
+
+/**
+ * This Function Will returns an Object of All datatypes counts
+ * @returns {Object}
+ */
+Object.prototype.info = function(){
+    let stringCounter = 0
+    let numberCounter = 0
+    let booleanCounter = 0
+    let objectCounter = 0
+    let arrayCounter = 0
+    for(let k in this){
+        if(k !=="info"){
+            if(typeof this[k] == "string"){
+                stringCounter++
+            }
+            else if(typeof this[k] =="number"){
+                numberCounter++
+            }
+            else if(typeof this[k] == "boolean"){
+                booleanCounter++
+            }else if(Array.isArray(this[k])){
+                arrayCounter++
+            }else if(typeof this[k] == "object"){
+                objectCounter++
+            }
+            else{
+                // nothing
+            }
+        }
+    } 
+    return {
+        strings: stringCounter,
+        numbers: numberCounter,
+        arrys: arrayCounter,
+        booleans: booleanCounter,
+        objects: objectCounter
+    }
+}
+const foo ={
+    t:5,
+    g: "str",
+    b: false,
+    h:[4,5],
+    o:{
+        r:5,
+        e:1
+    },
+    f:{
+        e:2
+    }
+}
+console.log(foo.info())
+//- loop object:
+// for in
+for(let k in foo){
+    console.log(k, foo[k])
+}
+Object.entries(foo).forEach(v=>{
+    console.log(v)
+})
+Object.values(foo).forEach(v=>{
+    console.log(v)
+})
+Object.keys(foo).forEach(k=>{
+    console.log(k)
+})
